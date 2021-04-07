3b10b805f820a819fac0a322c74922cc3c125a73
geneh0 <52678798+geneh0@users.noreply.github.com>
2021-04-06 22:09:45 -0700

    added more decimal places

--- app.R
+++ app.R
@@ -43,9 +43,9 @@ argonSidebar <- argonDashSidebar(
             icon = icon("fas fa-tshirt", lib = 'font-awesome'),
             "The t Distribution"),
         # argonSidebarItem(
-            # tabName = "03_chi2",
-            # icon = icon("fas fa-user-graduate", lib = 'font-awesome'),
-            # "The chi-squared Distribution")
+        # tabName = "03_chi2",
+        # icon = icon("fas fa-user-graduate", lib = 'font-awesome'),
+        # "The chi-squared Distribution")
         ),
     br())
 
@@ -206,6 +206,60 @@ ui <- argonDashPage(
                         plotOutput('tPlot')
                     )
                 )
+            ), # end argonTabItem
+            # chi2 Distribution ----
+            argonTabItem(
+                tabName = '03_chi2',
+                argonH1("t Distribution",
+                        display = 2),
+                argonRow(
+                    argonCard(title = argonH1('Inputs',
+                                              display = 4),
+                              width = 4,
+                              # t tail type buttons
+                              argonRow(
+                                  radioButtons(
+                                      "chi2_tail", "Type",
+                                      choices = c(
+                                          "Left-Tailed" = "left",
+                                          "Right-Tailed" = "right"
+                                      ),
+                                      selected = "left"
+                                  )
+                              ),
+                              argonRow(
+                                  numericInput(
+                                      "chi2df",
+                                      "Enter Degrees of Freedom",
+                                      value = 2,
+                                      min = 2,
+                                      max = Inf,
+                                      step = 1
+                                  )
+                              ),
+                              selectInput(
+                                  'chi2Curve_att',
+                                  label = 'Select Curve Input Attribute',
+                                  choices = c(
+                                      'chi2 score' = 'chi2_value_select',
+                                      'area under curve' = 'chi2_auc_select'
+                                  ),
+                                  width = '100%'
+                              ),
+                              uiOutput('chi2Att',
+                                       width = '100%'),
+                              uiOutput('chi2AutoCalc',
+                                       width = '100%')
+                              # end argonRow
+                    ),  # end argonCard
+
+                    argonCard(
+                        title = argonH1('Plot',
+                                        display = 4),
+                        width = 8,
+                        plotOutput('chi2Plot')
+                    )
+                )
             ) # end argonTabItem
         ) # end argonTabItems
     ), # end argonDashBody
@@ -223,6 +277,7 @@ server <- function(input, output, session) {
 
     output$normAtt <- renderUI({
         if(input$normCurve_att == 'z_value_select'){
+            print('stop')
             numericInput(
                 'z_value',
                 label = NULL,
@@ -249,7 +304,7 @@ server <- function(input, output, session) {
                   tail_type() == 'middle' ~ qnorm(0.50 + auc/2),
                   tail_type() == 'two' ~ qnorm(auc/2)
         ) %>%
-            round(4)
+            round(7)
     }
 
     calc_z_auc <- function(z_val){
@@ -260,14 +315,16 @@ server <- function(input, output, session) {
                       pnorm(-abs(z_score())),
                   tail_type() == 'two' ~ 2 * pnorm(-abs(z_score())),
                   TRUE ~ 0) %>%
-            round(4)
+            round(7)
     }
 
 
     z_score <- reactive({
         if(input$normCurve_att == 'z_value_select'){
+
+            print(input$z_value)
             input$z_value %>%
-                round(4)
+                round(7)
         }
         else if(input$normCurve_att == 'auc_select'){
             calc_z_score(norm_auc_react())
@@ -277,11 +334,15 @@ server <- function(input, output, session) {
 
     norm_auc_react <- reactive({
         if(input$normCurve_att == 'z_value_select'){
+
+            print(z_score())
             calc_z_auc(z_score())
+
+
         }
         else if(input$normCurve_att == 'auc_select'){
             input$norm_auc %>%
-                round(4)
+                round(7)
         }
     })
 
@@ -295,7 +356,7 @@ server <- function(input, output, session) {
 
             HTML(paste0(argonH1("Area Under Curve (AUC):",
                                 display = 4),
-                        h4(round(auc, 3))))
+                        h4(round(auc, 5))))
         } else if(input$normCurve_att == 'auc_select'){
             if(between(norm_auc_react(), 0, 1)){
                 z_value <- z_score()
@@ -396,33 +457,34 @@ server <- function(input, output, session) {
                                               lower.tail = FALSE),
                   # for t-val of middle, want half of prob to be above 50%
                   # then add 50%
-                  t_tail_type() == 'middle' ~ qt(auc/2 + .5, df = df) - qt(auc/2, df = df),
+                  t_tail_type() == 'middle' ~ qt(auc/2 + .5, df = df),
                   t_tail_type() == 'two' ~ qt(auc/2, df = df)
         ) %>%
-            round(4)
+            round(7)
     }
 
     calc_t_auc <- function(t_star, df){
         case_when(t_tail_type() == 'left' ~ pt(t_star, df = df),
                   t_tail_type() == 'right' ~ pt(t_star, df = df,
                                               lower.tail = FALSE),
-                  t_tail_type() == 'middle' ~ pt(t_star, df = df) -
-                      pt(-(t_star), df = df),
-                  t_tail_type() == 'two' ~ pt(t_star, df = df) + pt(-t_star, df = df),
+                  t_tail_type() == 'middle' ~ pt(abs(t_star), df = df) -
+                      pt(-abs(t_star), df = df),
+                  t_tail_type() == 'two' ~ 2 * pt(t_star, df = df),
                   TRUE ~ 0) %>%
-            round(4)
+            round(7)
     }
 
 
     t_score <- reactive({
         if(input$tCurve_att == 't_value_select'){
             input$t_value %>%
-                round(4)
+                round(7)
         }
         else if(input$tCurve_att == 'auc_select'){
             calc_t_score(t_auc_react(), t_df())
         }
     })
+
 
 
 
@@ -432,7 +494,7 @@ server <- function(input, output, session) {
         }
         else if(input$tCurve_att == 'auc_select'){
             input$t_auc %>%
-                round(4)
+                round(7)
         }
     })
 
@@ -444,12 +506,12 @@ server <- function(input, output, session) {
 
             HTML(paste0(argonH1("Area Under Curve (AUC):",
                                 display = 4),
-                        h4(round(auc, 3))))
+                        h4(round(auc, 5))))
         } else if(input$tCurve_att == 'auc_select'){
             if(between(t_auc_react(), 0, 1)){
 
                 t_value <- t_score() %>%
-                    round(3)
+                    round(7)
 
             }
             if(is.finite(t_value)){
@@ -512,6 +574,155 @@ server <- function(input, output, session) {
                               fill = 'white',
                               xlim = c(-abs(t_score()), abs(t_score())))
             } else NULL
+    })
+
+    # chi2 distribution ----
+
+    chi2_tail_type <- reactive({input$chi2_tail})
+    chi2_df <- reactive({input$chi2df})
+    chi2_dta <- data.frame(x = c(0, 100))
+
+
+    output$chi2Att <- renderUI({
+        if(input$chi2Curve_att == 'chi2_value_select'){
+            numericInput(
+                'chi2_value',
+                label = NULL,
+                value = 0,
+                min = 0,
+                max = Inf,
+                width = '100%')
+        } else if(input$chi2Curve_att == 'chi2_auc_select'){
+            numericInput(
+                'chi2_auc',
+                label = NULL,
+                value = 0,
+                min = 0,
+                max = 1,
+                width = '100%')
+        }
+    })
+
+    calc_chi2_score <- function(auc, df){
+        case_when(chi2_tail_type() == 'left' ~ qchisq(auc, df = df),
+                  chi2_tail_type() == 'right' ~ qchisq(auc, df = df,
+                                                lower.tail = FALSE),
+                  # chi2_tail_type() == 'exact' ~ dchisq()
+                  TRUE ~ 0
+        ) %>%
+            round(7)
+    }
+
+    calc_chi2_auc <- function(chi2_star, df){
+        case_when(chi2_tail_type() == 'left' ~ pchisq(chi2_star, df = df),
+                  chi2_tail_type() == 'right' ~ pchisq(chi2_star, df = df,
+                                                       lower.tail = FALSE),
+                  TRUE ~ 0) %>%
+            round(7)
+    }
+
+
+    chi2_score <- reactive({
+        if(input$chi2Curve_att == 'chi2_value_select'){
+            input$chi2_value %>%
+                round(7)
+        }
+        else if(input$chi2Curve_att == 'chi2_auc_select'){
+            calc_chi2_score(chi2_auc_react(), t_df())
+        }
+    })
+
+
+
+    chi2_auc_react <- reactive({
+        if(input$chi2Curve_att == 'chi2_value_select'){
+
+            calc_chi2_auc(chi2_score(), chi2_df())
+        }
+        else if(input$chi2Curve_att == 'chi2_auc_select'){
+
+            input$chi2_auc %>%
+                round(7)
+        }
+    })
+
+
+    output$chi2AutoCalc <- renderUI({
+
+        if(input$chi2Curve_att == 'chi2_value_select'){
+
+            auc <- chi2_auc_react()
+
+            HTML(paste0(argonH1("Area Under Curve (AUC):",
+                                display = 4),
+                        h4(round(auc, 5))))
+        } else if(input$chi2Curve_att == 'chi2_auc_select'){
+            if(between(chi2_auc_react(), 0, 1)){
+
+                chi2_value <- chi2_score() %>%
+                    round(7)
+
+
+            }
+            if(is.finite(chi2_value)){
+
+                if(chi2_tail_type() %in% c('middle', 'two') & chi2_score() != 0){
+                    chi2_value <- paste('+/-', abs(chi2_value))
+                }
+
+                HTML(paste0(argonH1(withMathJax("$\\chi^2$ score:"),
+                                    display = 4),
+                            h4(chi2_value)))
+            } else {
+                "Please enter a value between 0 and 1. "
+            }
+        }
+    })
+
+    output$chi2Plot <- renderPlot({
+
+
+        lower_lim <- case_when(chi2_tail_type() %in% c('left', 'right') ~ 0,
+                               TRUE ~ 0)
+        upper_lim <- case_when(chi2_tail_type() %in% c('right', 'left') ~ max(50, chi2_df() * 2),
+                               TRUE ~ 0)
+
+        lower_lim_area <- case_when(chi2_tail_type() == 'left' ~ as.double(0),
+                                    chi2_tail_type() ==  'right' ~ as.double(chi2_score()),
+                                    TRUE ~ 0)
+        upper_lim_area <- case_when(chi2_tail_type() == 'left' ~ as.double(chi2_score()),
+                                    chi2_tail_type() ==  'right' ~ upper_lim,
+                                    TRUE ~ 0)
+
+        label <- HTML(paste0('chi2 value: ', chi2_score(), '\n',
+                             'df: ', chi2_df(), '\n',
+                             'auc: ', chi2_auc_react()))
+
+        print(c(lower_lim, upper_lim))
+
+        print(c(lower_lim_area, upper_lim_area))
+
+
+        ggplot(data = chi2_dta, aes(x = x)) +
+            stat_function(fun = ~ dchisq(.x, df = chi2_df()),
+                          geom = 'area',
+                          xlim = c(lower_lim_area, upper_lim_area),
+                          fill = "#FDB515") +
+            stat_function(fun = ~ dchisq(.x, df = chi2_df()),
+                          geom = 'line',
+                          size = 1.5,
+                          xlim = c(lower_lim, upper_lim),
+                          color = '#3B7EA1') +
+            xlim(0, upper_lim) +
+            annotate('text',
+                     label = label,
+                     x = Inf, y = Inf,
+                     hjust = 1,
+                     vjust = 2,
+                     color = 'red',
+                     size = 6) +
+            labs(x = 't score') +
+            theme_minimal()
     })
 }
 
